776ffdca165c61b5124ca92349149adae72756d6
Abdulrahaman Ridwan Ayinla <abdulrahamanr657@gmail.com>
2025-07-16 04:20:12 -0700

    Add: completed the crew page

--- script.js
+++ script.js
@@ -3,13 +3,6 @@
 //selecting element 
 const navBarEl = document.querySelector('.nav-bar');
 const navEl = document.querySelector('nav');
-const locationTabContainerEl = document.querySelector('.locations__tab-container');
-const tabs = document.querySelectorAll('.locations__tab');
-const tabContents = document.querySelectorAll('.locations__content');
-const imageContents = document.querySelectorAll('.image__content')
-
-
-console.log(navBarEl);
 
 
 //activating the navigation for mobile users
@@ -31,22 +24,4 @@ navBarEl.addEventListener('click', function() {
     console.log('clicked');
 })
 
-//handling location clicked
 
-locationTabContainerEl.addEventListener('click', function(e) {
-  const clicked = e.target.closest('.locations__tab');
-  console.log(clicked);
-
-  //Guard clause
-  if (!clicked) return
-  //remove active tab
-  tabs.forEach(t => t.classList.remove('locations__tab--active')); 
-  clicked.classList.add('locations__tab--active');
-  tabContents.forEach(c => c.classList.remove('locations__content--active'));
-  imageContents.forEach(c => c.classList.remove('image--active'))
-  
-  //Activate content area
-  document
-  .querySelector(`.locations__content--${clicked.dataset.tab}`).classList.add('locations__content--active');
-  document.querySelector(`.image--${clicked.dataset.tab}`).classList.add('image--active');
-})
---
+++ scripts/destination.js
@@ -0,0 +1,25 @@
+const locationTabContainerEl = document.querySelector('.locations__tab-container');
+const tabs = document.querySelectorAll('.locations__tab');
+const tabContents = document.querySelectorAll('.locations__content');
+const imageContents = document.querySelectorAll('.image__content')
+
+/////////////////////////////////////////////
+//////////////////////////////////////////////////
+//handling location clicked
+locationTabContainerEl.addEventListener('click', function(e) {
+  const clicked = e.target.closest('.locations__tab');
+  console.log(clicked);
+
+  //Guard clause
+  if (!clicked) return
+  //remove active tab
+  tabs.forEach(t => t.classList.remove('locations__tab--active')); 
+  clicked.classList.add('locations__tab--active');
+  tabContents.forEach(c => c.classList.remove('locations__content--active'));
+  imageContents.forEach(c => c.classList.remove('image--active'))
+  
+  //Activate content area
+  document
+  .querySelector(`.locations__content--${clicked.dataset.tab}`).classList.add('locations__content--active');
+  document.querySelector(`.image--${clicked.dataset.tab}`).classList.add('image--active');
+})
